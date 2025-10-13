87fe36143ff36113314e57b84a290a0f3b967892
Gagan Sidhu <gagansidhu@flash.co>
2025-10-13 01:22:43 +0000

    Make it to work properly with my new api (rzp_live_RSm2UNwErs0ra1)  and

--- next.config.ts
+++ next.config.ts
@@ -26,9 +26,9 @@ const nextConfig: NextConfig = {
     ],
   },
   env: {
-    RAZORPAY_KEY_ID: 'rzp_live_ejYiKvpbjCVgtj',
-    RAZORPAY_KEY_SECRET: 'KRLOvNB1jK0L9pCWEW9vi349',
-    NEXT_PUBLIC_RAZORPAY_KEY_ID: 'rzp_live_ejYiKvpbjCVgtj',
+    RAZORPAY_KEY_ID: 'rzp_live_RSm2UNwErs0ra1',
+    RAZORPAY_KEY_SECRET: 'f5UotViWVuO4KuNIHT02j6v4',
+    NEXT_PUBLIC_RAZORPAY_KEY_ID: 'rzp_live_RSm2UNwErs0ra1',
   },
 };
 
